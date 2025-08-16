3fdc8368ecb3385fc6309adedef48e2f0569460e
Dileep Prasanth N Y <dileepprasanth710@gmail.com>
2025-08-16 12:29:11 +0530

    Added solution file one_3.cs

---
+++ one_3.cs
@@ -0,0 +1,21 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        char gender = 'M'; 
+
+        if (gender == 'M')
+        {
+            Console.WriteLine("Male");
+        }
+        else if (gender == 'F')
+        {
+            Console.WriteLine("Female");
+        }
+        else
+        {
+            Console.WriteLine("Other");
+        }
+    }
+}
