3ce5d4c498cb3ae9d520be5737cc4661d6553f85
Dileep Prasanth N Y <dileepprasanth710@gmail.com>
2025-08-16 12:38:10 +0530

    Added solution file one_5.cs

---
+++ one_5.cs
@@ -0,0 +1,17 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        bool isLoginSuccessful = true;
+
+        if (isLoginSuccessful)
+        {
+            Console.WriteLine("Login successful");
+        }
+        else
+        {
+            Console.WriteLine("Access denied");
+        }
+    }
+}
